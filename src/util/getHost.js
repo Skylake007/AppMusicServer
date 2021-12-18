e0023c4c23c0291dccdf93e623c71d48681f2831
Tan Nguyen <tannguyenvnkg@gmail.com>
2021-12-18 11:13:54 +0700

    rebuild

--- src/util/getHost.js
+++ src/util/getHost.js
@@ -1,8 +1,8 @@
 function getHost(req){
     // const host = req.headers.host;
-    // const host = '192.168.2.3:3000'; // TunsOggy
+    const host = '192.168.2.3:3000'; // TunsOggy
     // const host = ''; // Nghai Vui Tinh
-    const host = '192.168.0.31:3000'; // Sipagu
+    // const host = '192.168.0.31:3000'; // Sipagu
     return host;
 }
 
